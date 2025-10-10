0455b136935b6af8118da507a468c821f1258785
9Tanaka <Thanakit.ji@kkumail.com>
2025-10-10 17:33:21 +0700

    newest

--- js/hamburger.js
+++ js/hamburger.js
@@ -5,3 +5,20 @@ hamburger.addEventListener("click", () => {
     navElement.classList.toggle("active");
     hamburger.classList.toggle("open");
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+            if (window.innerWidth <= 768) {
+                const dropdownSubmenus = document.querySelectorAll('.dropdown-submenu');
+                dropdownSubmenus.forEach(submenu => {
+                    submenu.addEventListener('click', function(e) {
+                        const dropdownItem = this.querySelector('.dropdown-item');
+                        if (e.target === dropdownItem) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            this.classList.toggle('show');
+                        }
+                    });
+                });
+            }
+        });
